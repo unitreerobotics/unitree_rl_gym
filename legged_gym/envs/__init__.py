4a35df715277f72132c22a48fd5c2c04a54b9ff4
brook-bee <1215918085@qq.com>
2024-01-12 10:18:20 +0800

    add h1

--- legged_gym/envs/__init__.py
+++ legged_gym/envs/__init__.py
@@ -2,8 +2,11 @@
 from legged_gym import LEGGED_GYM_ROOT_DIR, LEGGED_GYM_ENVS_DIR
 
 from legged_gym.envs.go2.go2_config import GO2RoughCfg, GO2RoughCfgPPO
+from legged_gym.envs.h1.h1_config import H1RoughCfg, H1RoughCfgPPO
 from .base.legged_robot import LeggedRobot
 
 from legged_gym.utils.task_registry import task_registry
-task_registry.register( "go2", LeggedRobot, GO2RoughCfg(), GO2RoughCfgPPO() )
+task_registry.register( "go2", LeggedRobot, GO2RoughCfg(), GO2RoughCfgPPO())
+task_registry.register( "h1", LeggedRobot, H1RoughCfg(), H1RoughCfgPPO())
+
 
--- legged_gym/envs/h1/h1_config.py
+++ legged_gym/envs/h1/h1_config.py
@@ -1,27 +1,29 @@
 from legged_gym.envs.base.legged_robot_config import LeggedRobotCfg, LeggedRobotCfgPPO
 
-class GO2RoughCfg( LeggedRobotCfg ):
+class H1RoughCfg( LeggedRobotCfg ):
     class init_state( LeggedRobotCfg.init_state ):
-        pos = [0.0, 0.0, 0.42] # x,y,z [m]
+        pos = [0.0, 0.0, 1.0] # x,y,z [m]
         default_joint_angles = { # = target angles [rad] when action = 0.0
-            'FL_hip_joint': 0.1,   # [rad]
-            'RL_hip_joint': 0.1,   # [rad]
-            'FR_hip_joint': -0.1 ,  # [rad]
-            'RR_hip_joint': -0.1,   # [rad]
-
-            'FL_thigh_joint': 0.8,     # [rad]
-            'RL_thigh_joint': 1.,   # [rad]
-            'FR_thigh_joint': 0.8,     # [rad]
-            'RR_thigh_joint': 1.,   # [rad]
-
-            'FL_calf_joint': -1.5,   # [rad]
-            'RL_calf_joint': -1.5,    # [rad]
-            'FR_calf_joint': -1.5,  # [rad]
-            'RR_calf_joint': -1.5,    # [rad]
+           'left_hip_yaw_joint' : 0. ,   
+           'left_hip_roll_joint' : 0,               
+           'left_hip_pitch_joint' : -0.4,         
+           'left_knee_joint' : 0.8,       
+           'left_ankle_joint' : -0.4,     
+           'right_hip_yaw_joint' : 0., 
+           'right_hip_roll_joint' : 0, 
+           'right_hip_pitch_joint' : -0.4,                                       
+           'right_knee_joint' : 0.8,                                             
+           'right_ankle_joint' : -0.4,                                     
+           'torso_joint' : 0., 
+           'left_shoulder_pitch_joint' : 0., 
+           'left_shoulder_roll_joint' : 0, 
+           'left_shoulder_yaw_joint' : 0.,
+           'left_elbow_joint'  : 0.,
+           'right_shoulder_pitch_joint' : 0.,
+           'right_shoulder_roll_joint' : 0.0,
+           'right_shoulder_yaw_joint' : 0.,
+           'right_elbow_joint' : 0.,
         }
-    class env(LeggedRobotCfg.env):
-        num_observations = 105
-        num_actions = 10
 
     class control( LeggedRobotCfg.control ):
         # PD Drive parameters:
@@ -34,21 +36,21 @@ class GO2RoughCfg( LeggedRobotCfg ):
         decimation = 4
 
     class asset( LeggedRobotCfg.asset ):
-        file = '{LEGGED_GYM_ROOT_DIR}/resources/robots/go2/urdf/go2.urdf'
-        name = "go2"
-        foot_name = "foot"
+        file = '{LEGGED_GYM_ROOT_DIR}/resources/robots/h1/urdf/h1.urdf'
+        name = "h1"
+        foot_name = "ankle"
         penalize_contacts_on = ["thigh", "calf"]
         terminate_after_contacts_on = ["base"]
         self_collisions = 1 # 1 to disable, 0 to enable...bitwise filter
   
     class rewards( LeggedRobotCfg.rewards ):
         soft_dof_pos_limit = 0.9
-        base_height_target = 0.25
+        base_height_target = 0.98
         class scales( LeggedRobotCfg.rewards.scales ):
             torques = -0.0002
             dof_pos_limits = -10.0
 
-class GO2RoughCfgPPO( LeggedRobotCfgPPO ):
+class H1RoughCfgPPO( LeggedRobotCfgPPO ):
     class algorithm( LeggedRobotCfgPPO.algorithm ):
         entropy_coef = 0.01
     class runner( LeggedRobotCfgPPO.runner ):
