17de59ad94787289faec6c2d578764f7311bc82a
craipy <cpy_3566@163.com>
2024-07-18 09:45:05 +0800

    add g1 task

--- legged_gym/envs/__init__.py
+++ legged_gym/envs/__init__.py
@@ -3,10 +3,11 @@ from legged_gym import LEGGED_GYM_ROOT_DIR, LEGGED_GYM_ENVS_DIR
 
 from legged_gym.envs.go2.go2_config import GO2RoughCfg, GO2RoughCfgPPO
 from legged_gym.envs.h1.h1_config import H1RoughCfg, H1RoughCfgPPO
+from legged_gym.envs.g1.g1_config import G1RoughCfg, G1RoughCfgPPO
 from .base.legged_robot import LeggedRobot
 
 from legged_gym.utils.task_registry import task_registry
 task_registry.register( "go2", LeggedRobot, GO2RoughCfg(), GO2RoughCfgPPO())
 task_registry.register( "h1", LeggedRobot, H1RoughCfg(), H1RoughCfgPPO())
-
+task_registry.register( "g1", LeggedRobot, G1RoughCfg(), G1RoughCfgPPO())
 
